49ee72491a0b14ba2db6590170db0935e939987c
Nathaniel D. Hoffman <dene@cmu.edu>
2021-11-03 21:26:54 -0400

    first commit

---
+++ PSelector.py
@@ -0,0 +1,12 @@
+import uproot
+import numpy as np
+import argparse
+
+def get_source(rootfile):
+    text = f"""#ifndef selector_name_h
+"""
+    return text
+
+
+if __name__ == "__main__":
+    print(get_source(None))
