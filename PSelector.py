75df10c9b7d5495c118643551add54640ad1a933
Dene Hoffman <denehoffman@me.com>
2021-11-05 02:14:10 -0400

    add working registers for basic operations, update demo.yaml

--- PSelector.py
+++ PSelector.py
@@ -1,11 +1,18 @@
-import uproot
 import ROOT as root
 import numpy as np
 import argparse
 import re
+import yaml
+from pprint import pprint
 from pathlib import Path
 from particle import Particle, ParticleNotFound, InvalidParticle
+import Register
 
+###############################################################################################################
+#                                                                                                             #
+#                                          Write Header Method                                                #
+#                                                                                                             #
+###############################################################################################################
 def get_particle_map(path):
     f = root.TFile(path)
     treename = str(f.GetListOfKeys()[0].GetName())
@@ -41,7 +48,7 @@ def get_particle_map(path):
         output_sorted.append(sorted(step_list, key=lambda d: d.get("index")))
     return treename, branches, output_sorted
 
-def get_header(treename, branches, particle_map, config=None, basename="default"):
+def get_header(register, treename, branches, particle_map, config=None, basename="default"):
     header_text = f"""#ifndef DSelector_{basename}_h
 #define DSelector_{basename}_h
 
@@ -100,14 +107,15 @@ class DSelector_{basename} : public DSelector {{
                 header_text += f"        DNeutralTrackHypothesis* d{particle.get('name')}Wrapper;\n"
         header_text += "\n"
 
-    # add histograms
-    header_text += """        // DEFINE YOUR HISTOGRAMS HERE
-        // EXAMPLES:
-        TH1I* dHist_MissingMassSquared;
-        TH1I* dHist_BeamEnergy;
+    ###################################### add histograms
+    header_text += "        // DEFINE YOUR HISTOGRAMS HERE\n"
+    for hist_name, hist in register.get("histograms").items():
+        if hist.y:
+            header_text += f"        TH2I* dHist_{hist_name};\n"
+        else:
+            header_text += f"        TH1I* dHist_{hist_name};\n"
 
-"""
-    header_text += f"""   ClassDef(DSelector_{basename}, 0);
+    header_text += f"""\n    ClassDef(DSelector_{basename}, 0);
 }};
 
 void DSelector_{basename}::Get_ComboWrappers(void) {{
@@ -137,7 +145,13 @@ void DSelector_{basename}::Get_ComboWrappers(void) {{
 
     return header_text
 
-def get_source(treename, branches, particle_map, config=None, basename="default"):
+
+###############################################################################################################
+#                                                                                                             #
+#                                          Write Source Method                                                #
+#                                                                                                             #
+###############################################################################################################
+def get_source(register, treename, branches, particle_map, config=None, basename="default"):
     source_text = f"""#include "DSelector_{basename}.h"
 
 void DSelector_{basename}::Init(TTree *locTree) {{
@@ -155,7 +169,7 @@ void DSelector_{basename}::Init(TTree *locTree) {{
     Get_ComboWrappers();
     dPreviousRunNumber = 0;
 """
-    # add analysis action stuff here if we want that
+    ###################################### add analysis action stuff here if we want that
     # false/true is measured/kinfit
     source_text += f"""    dAnalysisActions.push_back(new DHistogramAction_ParticleID(dComboWrapper, false));
     dAnalysisActions.push_back(new DHistogramAction_PIDFOM(dComboWrapper));
@@ -166,15 +180,18 @@ void DSelector_{basename}::Init(TTree *locTree) {{
     Initialize_Actions();
 
 """
-    # add histograms
-    source_text += """    // DEFINE HISTOGRAMS HERE
-    // EXAMPLES:
-    dHist_MissingMassSquared = new TH1I("MissingMassSquared", ";Missing Mass Squared (GeV/c^{2})^{2}", 600, -0.06, 0.06);
-    dHist_BeamEnergy = new TH1I("BeamEnergy", ";Beam Energy (GeV)", 600, 0.0, 12.0);
+    ###################################### add histograms
+    source_text += "    // DEFINE HISTOGRAMS HERE\n"
+    for hist_name, hist in register.get("histograms").items():
+        if hist.y:
+            source_text += f"    dHist_{hist_name} = new TH2I(\"{hist_name}\", \"{hist.get_label_string()}\", {hist.xbins}, {hist.xrange[0]}, {hist.xrange[1]}, {hist.ybins}, {hist.yrange[0]}, {hist.yrange[1]});\n"
+        else:
+            source_text += f"    dHist_{hist_name} = new TH1I(\"{hist_name}\", \"{hist.get_label_string()}\", {hist.xbins}, {hist.xrange[0]}, {hist.xrange[1]});\n"
+    
+    source_text += "\n"
 
-"""
-    # custom main tree stuff
-    # custom flat tree stuff
+    ###################################### custom main tree stuff
+    ###################################### custom flat tree stuff
 
     source_text += f"""
     dIsMC = (dTreeInterface->Get_Branch("MCWeight") != NULL);
@@ -195,24 +212,29 @@ Bool_t DSelector_{basename}::Process(Long64_t locEntry) {{
     }}
 
 """
-    # uniqueness tracking
     source_text += f"""
     Reset_Actions_NewEvent();
-
-    set<Int_t> locUsedSoFar_BeamEnergy;
-    set<map<Particle_t, set<Int_t>>> locUsedSoFar_MissingMass;
-
 """
-    # fill custom output branches
-    # loop over combos
+    ###################################### uniqueness tracking
+    for uniq_name, uniq_info in register.get("uniqueness").items():
+        if uniq_info.particles:
+            if len(uniq_info.particles) == 1: # the "all" keyword fails this test intentionally!
+                source_text += f"    set<Int_t> locUsedSoFar_{uniq_name};\n"
+            else:
+                source_text += f"    set<map<Particle_t, set<Int_t>>> locUsedSoFar_{uniq_name};\n"
+
+    ###################################### fill custom output branches
+    ###################################### loop over combos
     source_text += f"""
+
     for(UInt_t loc_i = 0; loc_i < Get_NumCombos(); ++loc_i) {{
+
         dComboWrapper->Set_ComboIndex(loc_i);
         if(dComboWrapper->Get_IsComboCut()) {{
             continue;
         }}
 
-    // GET PARTICLE INDICES FOR UNIQUENESS TRACKING
+        // GET PARTICLE INDICES FOR UNIQUENESS TRACKING
 """
     for step_index, step_contents in enumerate(particle_map):
         source_text += f"        // Step {step_index}\n"
@@ -233,7 +255,7 @@ Bool_t DSelector_{basename}::Process(Long64_t locEntry) {{
                 source_text += f"        Int_t loc{particle.get('name')}NeutralID = d{particle.get('name')}Wrapper->Get_NeutralID();\n" 
         source_text += "\n"
 
-    # Get four-vectors
+    ###################################### Get four-vectors
     for step_index, step_contents in enumerate(particle_map):
         source_text += f"        // Step {step_index}\n"
         for particle in step_contents:
@@ -260,14 +282,14 @@ Bool_t DSelector_{basename}::Process(Long64_t locEntry) {{
                 source_text += f"        TLorentzVector loc{particle.get('name')}X4_Measured = d{particle.get('name')}Wrapper->Get_X4_Measured();\n"
         source_text += "\n"
 
-    # Old Accidental Weighting
+    ###################################### Old Accidental Weighting
     n_out_of_time = -1
     num_from_treename = re.search("_B(\d+)", treename).group(1)
     if num_from_treename:
         n_out_of_time = int(num_from_treename)
     source_text += f"""
-    Double_t locDeltaT_RF = dAnalysisUtilities.Get_DeltaT_RF(Get_RunNumber(), locBeamX4_Measured, dComboWrapper);
-    Double_t locHistAccidWeightFactor = (fabs(locDeltaT_RF) > 0.5 * 4.008) ? -1/{2 * n_out_of_time} : 1;
+        Double_t locDeltaT_RF = dAnalysisUtilities.Get_DeltaT_RF(Get_RunNumber(), locBeamX4_Measured, dComboWrapper);
+        Double_t locHistAccidWeightFactor = (fabs(locDeltaT_RF) > 0.5 * 4.008) ? -1/{2 * n_out_of_time} : 1;
 
 """
 
@@ -289,11 +311,9 @@ Bool_t DSelector_{basename}::Process(Long64_t locEntry) {{
 
 """
     '''
-    # Do Cuts
-
     # Missing Mass Squared Calculation
-    source_text += "    TLorentzVector locMissingP4_Measured = locBeamP4_Measured + dTargetP4;\n"
-    source_text += "    locMissingP4_Measured -= "
+    source_text += "        TLorentzVector locMissingP4_Measured = locBeamP4_Measured + dTargetP4;\n"
+    source_text += "        locMissingP4_Measured -= "
     isFirstFlag = True
     for step_index, step_contents in enumerate(particle_map):
         for particle in step_contents:
@@ -313,50 +333,88 @@ Bool_t DSelector_{basename}::Process(Long64_t locEntry) {{
                 isFirstFlag = False
                 source_text += f"loc{particle.get('name')}P4_Measured"
     source_text += ";\n"
-    source_text += "    double locMissingMassSquared = locMissingP4_Measured.M2();\n"
+    source_text += "        double locMissingMassSquared = locMissingP4_Measured.M2();\n"
 
+    # Define new variables
+    # Extra Code:
+    for code_line in register.get("code"):
+        source_text += f"        {code_line}\n"
+    # Do Cuts
     # Execute Analysis Actions
-    source_text += f"""
-        if(!Execute_Actions()) {{
+    source_text += """
+        if(!Execute_Actions()) {
             continue;
-        }}
+        }
 """
     # Fill custom output branches
 
     # Fill Histograms
-    source_text += """
-        if(locUsedSoFar_BeamEnergy.find(locBeamID) == locUsedSoFar_BeamEnergy.end()) {
-            dHist_BeamEnergy->Fill(locBeamP4.E());
-
-            locUsedSoFar_BeamEnergy.insert(locBeamID);
-        }
-        map<Particle_t, set<Int_t>> locUsedThisCombo_MissingMass;
-        """
-    for step_index, step_contents in enumerate(particle_map):
-        for particle in step_contents:
-            if not particle.get("particle"):
-                continue
-            elif particle.get("name") == "ComboBeam":
-                source_text += "        locUsedThisCombo_MissingMass[Unknown].insert(locBeamID);\n"
-            elif particle.get("name").startswith("Target"):
-                continue
-            elif particle.get("name").startswith("Decaying"):
-                continue
-            elif particle.get("name").startswith("Missing"):
-                continue
-            elif particle.get("particle").charge != 0:
-                source_text += f"        locUsedThisCombo_MissingMass[PDGtoPType({particle.get('pid')})].insert(loc{particle.get('name')}TrackID);\n"
-            else:
-                source_text += f"        locUsedThisCombo_MissingMass[PDGtoPType({particle.get('pid')})].insert(loc{particle.get('name')}NeutralID);\n"
-        source_text += "\n"
-    source_text += """
-    if(locUsedSoFar_MissingMass.find(locUsedThisCombo_MissingMass) == locUsedSoFar_MissingMass.end()) {
-        dHist_MissingMassSquared->Fill(locMissingMassSquared, locHistAccidWeightFactor);
-        }
-    }
-
-    Fill_NumCombosSurvivedHists();
+    for uniq_name, uniq_info in register.get("uniqueness").items():
+        if uniq_info.particles:
+            if len(uniq_info.particles) == 1: # the "all" keyword fails this test intentionally!
+                source_text += f"""
+        if(locUsedSoFar_{uniq_name}.find(loc{uniq_info.particles[0]}ID) == locUsedSoFar_{uniq_name}.end()) {{
 """
+                for hist_name in uniq_info.histograms:
+                    hist_info = register.get("histograms").get(hist_name)
+                    if hist_info.y:
+                        source_text += f"            dHist_{hist_name}->Fill({hist_info.x}, {hist_info.y}, {hist_info.weight});\n"
+                    else:
+                        source_text += f"            dHist_{hist_name}->Fill({hist_info.x}, {hist_info.weight});\n"
+                source_text += f"            locUsedSoFar_{uniq_name}.insert(loc{uniq_info.particles[0]}ID);        \n        }}\n"
+            elif uniq_info.particles.lower() == "all": # all particles
+                source_text += f"        map<Particle_t, set<Int_t>> locUsedThisCombo_{uniq_name};\n"
+                for step_index, step_contents in enumerate(particle_map):
+                    for particle in step_contents:
+                        if not particle.get("particle"):
+                            continue
+                        elif particle.get("name") == "ComboBeam":
+                            source_text += f"        locUsedThisCombo_{uniq_name}[Unknown].insert(locBeamID);\n"
+                        elif particle.get("name").startswith("Target"):
+                            continue
+                        elif particle.get("name").startswith("Decaying"):
+                            continue
+                        elif particle.get("name").startswith("Missing"):
+                            continue
+                        elif particle.get("particle").charge != 0:
+                            source_text += f"        locUsedThisCombo_{uniq_name}[PDGtoPType({particle.get('pid')})].insert(loc{particle.get('name')}TrackID);\n"
+                        else:
+                            source_text += f"        locUsedThisCombo_{uniq_name}[PDGtoPType({particle.get('pid')})].insert(loc{particle.get('name')}NeutralID);\n"
+                    source_text += "\n"
+                source_text += f"""
+        if(locUsedSoFar_{uniq_name}.find(locUsedThisCombo_{uniq_name}) == locUsedSoFar_{uniq_name}.end()) {{
+"""
+                for hist_name in uniq_info.histograms:
+                    hist_info = register.get("histograms").get(hist_name)
+                    if hist_info.y:
+                        source_text += f"            dHist_{hist_name}->Fill({hist_info.x}, {hist_info.y}, {hist_info.weight});\n"
+                    else:
+                        source_text += f"            dHist_{hist_name}->Fill({hist_info.x}, {hist_info.weight});\n"
+                source_text += f"            locUsedSoFar_{uniq_name}.insert(locUsedThisCombo{uniq_name});        \n        }}\n"
+            else: # custom but more than one particle
+                source_text += f"        map<Particle_t, set<Int_t>> locUsedThisCombo_{uniq_name};\n"
+                for tracked_particle in uniq_info.particles:
+                    if tracked_particle == "Beam":
+                        source_text += f"        locUsedThisCombo_{uniq_name}[Unknown].insert(locBeamID);\n"
+                    else:
+                        for particle in step_contents:
+                            if particle.get("name") == tracked_particle:
+                                if particle.get("particle").charge != 0:
+                                    source_text += f"        locUsedThisCombo_{uniq_name}[PDGtoPType({particle.get('pid')})].insert(loc{particle.get('name')}TrackID);\n"
+                                else:
+                                    source_text += f"        locUsedThisCombo_{uniq_name}[PDGtoPType({particle.get('pid')})].insert(loc{particle.get('name')}NeutralID);\n"
+                source_text += f"""
+    if(locUsedSoFar_{uniq_name}.find(locUsedThisCombo_{uniq_name}) == locUsedSoFar_{uniq_name}.end()) {{
+"""
+                for hist_name in uniq_info.histograms:
+                    hist_info = register.get("histograms").get(hist_name)
+                    if hist_info.y:
+                        source_text += f"            dHist_{hist_name}->Fill({hist_info.x}, {hist_info.y}, {hist_info.weight});\n"
+                    else:
+                        source_text += f"            dHist_{hist_name}->Fill({hist_info.x}, {hist_info.weight});\n"
+                source_text += f"            locUsedSoFar_{uniq_name}.insert(locUsedThisCombo{uniq_name});        \n       }}\n"
+
+    source_text += "    } // End of Combo Loop\n\n    Fill_NumCombosSurvivedHists();\n"
 
     # Fill output tree
     source_text += """
@@ -383,6 +441,13 @@ void DSelector_{basename}::Finalize(void) {{
 
     return source_text
 
+
+
+###############################################################################################################
+#                                                                                                             #
+#                                              Main Method                                                    #
+#                                                                                                             #
+###############################################################################################################
 if __name__ == "__main__":
     parser_desc = """
 #############################################################\n
@@ -419,8 +484,23 @@ if __name__ == "__main__":
     else:
         config_path = None
     print(f"Generating {str(output_source_path)} and {str(output_header_path)}...")
+    register_histogram = Register.HistogramRegister({})
+    register_uniqueness = Register.UniquenessRegister({}, register_histogram.get_hist_names())
+    register_code = Register.CodeRegister({})
     if config_path:
         print(f"Using {str(config_path)} to supplement DSelector generation")
+        with open(config_path, 'r') as stream:
+            try:
+                parsed_config = yaml.safe_load(stream)
+                register_histogram = Register.HistogramRegister(parsed_config)
+                register_uniqueness = Register.UniquenessRegister(parsed_config, register_histogram.get_hist_names())
+                register_code = Register.CodeRegister(parsed_config)
+            except yaml.YAMLError as exc:
+                print(exc)
+    register = {"histograms": register_histogram.histograms,
+                "uniqueness": register_uniqueness.uniqueness,
+                "code": register_code.code}
+
     with open(output_source_path, 'w') as source, open(output_header_path, 'w') as header:
-        source.write(get_source(treename, branches, particle_map, config=config_path, basename=basename))
-        header.write(get_header(treename, branches, particle_map, config=config_path, basename=basename))
+        source.write(get_source(register, treename, branches, particle_map, config=config_path, basename=basename))
+        header.write(get_header(register, treename, branches, particle_map, config=config_path, basename=basename))
