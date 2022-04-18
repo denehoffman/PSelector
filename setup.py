5c8db2052cc9ca2bc5cd773fcc43665d74bd31ed
denehoffman <denehoffman@me.com>
2022-04-18 17:30:10 -0400

    setup pselector like a package

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
-import os
-
+from pathlib import Path
+SRC = Path(__file__).parent / "src/pselector"
 setup(
-  name="pselector",
-  version="0.0.1",
-  author="Nathaniel Dene Hoffman",
-  author_email="dene@cmu.edu",
-  packages=find_packages("src"),
-  package_dir={"": "src"},
-  scripts=[],
-  install_requires=[],
-  zip_safe=False
+    name="pselector",
+    version="0.0.1",
+    author="Nathaniel Dene Hoffman",
+    author_email="dene@cmu.edu",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    scripts=[str(SRC / "MakePSelector")],
+    install_requires=["particle"],
+    zip_safe=False
 )
