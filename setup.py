23f9f685a6a33e41518641919901d2b7a6bad253
Nathaniel D. Hoffman <36977879+denehoffman@users.noreply.github.com>
2022-07-12 16:26:37 -0400

    Update setup.py
    add python version dependency for tomli

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from pathlib import Path
 SRC = Path(__file__).parent / "src/pselector"
 setup(
     name="pselector",
-    version="0.0.1",
+    version="0.0.2",
     author="Nathaniel Dene Hoffman",
     author_email="dene@cmu.edu",
     packages=find_packages("src"),
     package_dir={"": "src"},
     scripts=[str(SRC / "MakePSelector")],
-    install_requires=["particle", "tomli"],
+    install_requires=["particle", 'tomli; python_version < "3.11"'],
     zip_safe=False
 )
